237e72c340ec26fb61789a677595234ed2505fb4
Jiahao Li <jiahaoli0465@gmail.com>
2024-02-29 20:34:44 -0500

    allowing API users to filter the results based on optional filtering criteria, any or all of which can be passed in the query string:
    - ***name***: filter by company name: if the string “net” is passed in, this should find any company who name contains the word “net”, case-insensitive (so “Study Networks” should be included).
    - ***minEmployees***: filter to companies that have at least that number of employees.
    - ***maxEmployees***: filter to companies that have no more than that number of employees.
    - If the ***minEmployees*** parameter is greater than the ***maxEmployees*** parameter, respond with a 400 error with an appropriate message.

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,6 +2,14 @@ const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
+/**
+ * Generates SQL query for partial update based on the provided data and mapping.
+ *
+ * @param {Object} dataToUpdate - The data to be updated.
+ * @param {Object} jsToSql - The mapping of JavaScript object keys to SQL column names.
+ * @returns {Object} - An object containing the set columns and corresponding values for the SQL query.
+ * @throws {BadRequestError} - If no data is provided.
+ */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
